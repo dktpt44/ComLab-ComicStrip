507faed2052d6ce192e0ceb32f45f862093d3eda
dktpt44 <dhurba6don@gmail.com>
2022-03-05 17:44:04 +0400

    cool animations added

--- welcome.js
+++ welcome.js
@@ -1,15 +1,23 @@
 
+// to do something when the document is ready
+$(document).ready(() => {
+
+});
+
+
 $(".welcomeButton").on("click", (elem) => {
   let welcomeScreen = $(".welcomeScreen");
   // fade away the welcome screen
   micron.getEle("#me").interaction("squeeze").duration(".60").timing("ease-out");
   setTimeout(() => {
-    welcomeScreen.css({ "opacity": 0 });
-  }, 400);
+    // welcomeScreen.css({ "opacity": 0.1 });
+    welcomeScreen.addClass("a-rotate-out");
+  }, 200);
   setTimeout(() => {
     // hide the welcome screen
     welcomeScreen.hide();
     // show the comic body
-    $(".comicBody").fadeIn();
+    $(".comicBody").show();
+    $(".comicBody").addClass("a-journal");
   }, 1200);
 });
