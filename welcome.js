79bd9879ab27abf867242897cd366f2bf6e1f149
dktpt44 <dhurba6don@gmail.com>
2022-03-05 15:33:19 +0400

    comments added

--- welcome.js
+++ welcome.js
@@ -3,10 +3,11 @@ $(".welcomeButton").on("click", (elem) => {
   // fade away the welcome screen
   welcomeScreen.css({ "opacity": 0 });
   setTimeout(() => {
+    // hide the welcome screen
     welcomeScreen.hide();
+    // show the comic body
     $(".comicBody").fadeIn();
   }, 800);
 
 
-  $(".comicBody")
 });
