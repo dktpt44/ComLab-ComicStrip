75a502c05374ad779bceeb60624d1274b090e0d1
dktpt44 <dhurba6don@gmail.com>
2022-03-05 18:46:51 +0400

    animation improved

--- js/welcome.js
+++ js/welcome.js
@@ -8,18 +8,16 @@ $(document).ready(() => {
   // animating when the button on the welcome screen is clicked
   $(".welcomeButton").on("click", (elem) => {
     let welcomeScreen = $(".welcomeScreen");
-    // fade away the welcome screen
     micron.getEle("#me").interaction("squeeze").duration(".60").timing("ease-out");
     setTimeout(() => {
-      // welcomeScreen.css({ "opacity": 0.1 });
-      welcomeScreen.addClass("a-rotate-out");
+      welcomeScreen.addClass("animate__animated animate__zoomOutUp");
     }, 200);
     setTimeout(() => {
       // hide the welcome screen
       welcomeScreen.hide();
       // show the comic body
       $(".comicBody").show();
-      $(".comicBody").addClass("a-journal");
+      $(".comicBody").addClass("animate__animated animate__zoomInUp");
     }, 1200);
   });
 });
