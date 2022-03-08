3f50b6a1deeeda6e4290f826db4b37e31a317ff4
dktpt44 <dhurba6don@gmail.com>
2022-03-08 09:14:20 +0400

    flipbook made

--- js/welcome.js
+++ js/welcome.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
   }, 1500);
 
   // animating when the button on the welcome screen is clicked
-  $(".welcomeButton").on("click", (elem) => {
+  $($(".welcomeButton")[0]).on("click", (elem) => {
     let welcomeScreen = $(".welcomeScreen");
     micron.getEle("#me").interaction("squeeze").duration(".60").timing("ease-out");
     setTimeout(() => {
@@ -19,9 +19,28 @@ $(document).ready(() => {
       // show the comic body
       $(".comicbody").show();
       $(".comicbody").addClass("animate__animated animate__zoomInUp");
-      
+
     }, 1200);
   });
+
+  // animation when the other button is clicked on
+  $($(".welcomeButton")[1]).on("click", (elem) => {
+    let welcomeScreen = $(".welcomeScreen");
+    micron.getEle("#me2").interaction("squeeze").duration(".60").timing("ease-out");
+    setTimeout(() => {
+      welcomeScreen.addClass("animate__animated animate__zoomOutUp");
+    }, 200);
+    setTimeout(() => {
+      // hide the welcome screen
+      welcomeScreen.hide();
+      // show the comic body
+      $(".comicbodyflipbook").show();
+      $(".comicbodyflipbook").addClass("animate__animated animate__zoomInUp");
+
+    
+    }, 1200);
+  });
+
 });
 
 
